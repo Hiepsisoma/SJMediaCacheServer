58fddfba912d73d3dc9a8da762b90c1fdea51061
HungPT <hungpt@tiin.vn>
2024-10-17 10:31:32 +0700

    Fix: Resolve issue where the first TS segment was missing when playing M3U8 video

--- SJMediaCacheServer/Core/Asset/HLS/HLSAssetReader.m
+++ SJMediaCacheServer/Core/Asset/HLS/HLSAssetReader.m
@@ -101,17 +101,16 @@
                 break;
             case MCSDataTypeHLSTs: {
                 id<HLSAssetTsContent> content = [mAsset TsContentReadwriteForRequest:mRequest];
-                if      ( content == nil ) {
+                if      ( content == nil || content.totalLength == 0) {
+                    NSLog(@"HungPT HLSAssetReader prepare content == nil");
                     mReader = [MCSAssetHTTPContentReader.alloc initWithAsset:mAsset request:mRequest networkTaskPriority:_networkTaskPriority dataType:MCSDataTypeHLSTs delegate:self];
                 }
-                else if ( content.length == content.totalLength ||  content.totalLength == 0 ) {
-                    if (content.totalLength == 0 ){
-                        content.totalLength = content.length;
-                        content.rangeInAsset = NSMakeRange(0, content.length);
-                    }
+                else if ( content.length == content.totalLength) {
+                    NSLog(@"HungPT HLSAssetReader prepare content.length == content.totalLength content = %@ content.rangeInAsset.location = %lu content.rangeInAsset.location = %lu", content, (unsigned long)content.rangeInAsset.location, (unsigned long)content.rangeInAsset.length);
                     mReader = [MCSAssetFileContentReader.alloc initWithAsset:mAsset fileContent:content rangeInAsset:content.rangeInAsset delegate:self];
                 }
                 else {
+                    NSLog(@"HungPT HLSAssetReader prepare =========== content = %@ content.rangeInAsset.location = %lu content.rangeInAsset.location = %lu totalLength =  %lu", content, (unsigned long)content.rangeInAsset.location, (unsigned long)content.rangeInAsset.length, (unsigned long)content.totalLength);
                     mReader = [MCSAssetHTTPContentReader.alloc initWithAsset:mAsset request:mRequest rangeInAsset:content.rangeInAsset contentReadwrite:content networkTaskPriority:_networkTaskPriority dataType:MCSDataTypeHLSTs delegate:self];
                 }
             }
